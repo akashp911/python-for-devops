6246bbf5656439eecb8a81cd23f321383734c3f1
akashp911 <67469755+akashp911@users.noreply.github.com>
2026-05-02 09:22:16 +0530

    Create len-type.py
    test

---
+++ Built-in-functions/len-type.py
@@ -0,0 +1,10 @@
+# Tasks:
+
+# Find the number of elements in data using a built-in function.
+# Check the data type of data using a built-in function.
+# Store both results in variables and print them.
+
+data = ["apple", "banana", "cherry", "date"]
+
+print(data.len())
+print(data. 
